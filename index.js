fe83f86ab760e6b9d8601a3074ed077355eaf783
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-26 11:37:55 -0700

    implemented the sumItems function to give the sum of nested array values

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function sumItems(array) {
+
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let result = 0;
+  array.forEach((x) => {
+    if (Array.isArray(x)) {
+      result += sumItems(x);
+    } else {
+      result += x;
+    }
+  });
+  return result;
+};
+
+console.log(sumItems([1, 2, 3, 4, 5]));
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
+console.log(sumItems([[[[[[[1]]]]]]]));
 
 module.exports = sumItems;
